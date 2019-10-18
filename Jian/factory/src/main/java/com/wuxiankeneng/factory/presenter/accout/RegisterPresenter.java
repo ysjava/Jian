f8b531468c10a41a6bb57b20dc356820c3718cd2
neng <neng@email.com>
2019-10-18 13:19:52 +0800

    Update RegisterPresenter.java

--- Jian/factory/src/main/java/com/wuxiankeneng/factory/presenter/accout/RegisterPresenter.java
+++ Jian/factory/src/main/java/com/wuxiankeneng/factory/presenter/accout/RegisterPresenter.java
@@ -28,9 +28,21 @@ public class RegisterPresenter extends BasePresenter<RegisterContract.View>
         if (TextUtils.isEmpty(phone) || TextUtils.isEmpty(password)) {
             view.showError(R.string.account_or_password_error);
         } else {
-            RegisterModel model = new RegisterModel(phone, password, Account.getPushId(),"王八","9c49e380-636d-477a-884c-5983cdff03da");
+            RegisterModel model = new RegisterModel(phone, password, Account.getPushId(),getName(),"b271a08d-d5b8-4b15-8422-237315ae5380");
             AccountHelper.register(model, this);
         }
+    }
+
+    //从名字集合随机抽取一个
+    private String getName() {
+        String[] names = new String[]{
+                "杨大山", "杨小山", "杨前锋", "杨马褂", "杨二麻",
+                "赵破胆", "赵明灯", "赵灯笼", "赵几几", "赵三娃",
+                "李太保", "李大妈", "李大框", "李青米", "李好吗",
+                "钱老爷", "钱多多", "钱面看", "钱依琳", "钱哥哥"
+        };
+        int i = (int) (Math.random() * 19);
+        return names[i] + (int) (Math.random() * 1000);
     }
 
     @Override
